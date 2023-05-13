c75ab7e43cc1d78c6879c1b0dec12921b2dab325
Siddhesh-Shirawale <80307604+Siddhesh-Shirawale@users.noreply.github.com>
2023-05-14 00:55:54 +0530

    scroll view and flatlist done

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { useState } from "react";
 
 export default function App() {
-  const [showText, setShowText] = useState(false);
+  const [goals, setGoals] = useState([]);
+  const [goalName, setGoalName] = useState("");
 
-  const onPressButton = () => {
-    setShowText(!showText);
+  const onAddGoal = () => {
+    setGoals((currentGoals) => [
+      { text: goalName, key: Math.random().toString() },
+      ...currentGoals,
+    ]);
+  };
+
+  const handleInputChange = (enteredText) => {
+    setGoalName(enteredText);
+  };
+
+  const clearGoals = () => {
+    setGoals([]);
   };
 
   return (
     <View style={styles.appContainer}>
       <View style={styles.flexContainer}>
-        <TextInput style={styles.inputGroup} placeholder="Yout course goals" />
-        <Button style={{ borderRadius: 8 }} title="Add goals" />
+        <TextInput
+          style={styles.inputGroup}
+          placeholder="Your goals"
+          // value={goalName}
+          onChangeText={handleInputChange}
+        />
+
+        <Button
+          onPress={onAddGoal}
+          style={{ borderRadius: 8 }}
+          title="Add goals"
+          color="#191970"
+        />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <View
+          style={{
+            justifyContent: "center",
+            padding: 2,
+            borderRadius: 8,
+            width: "100%",
+          }}
+        >
+          <Button onPress={clearGoals} title="Clear" color="#d3d3d3" />
+        </View>
+        <FlatList
+          style={{ width: "100%" }}
+          data={goals}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{item?.text}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -23,18 +75,43 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputGroup: {
     borderWidth: 1,
     borderColor: "#555",
     padding: 8,
-    borderRadius: 8,
-    width: "80%",
+    width: "70%",
     marginRight: 8,
   },
   flexContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#555",
+    flex: 1,
+  },
+  goalsContainer: {
+    flex: 5,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 4,
+    backgroundColor: "#20b2aa",
+    color: "white",
+    // width: "100%",
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+  goalText: {
+    color: "white",
   },
 });
+
+// borderRadius on Text doesn't work in ios, so wrap around that element with view and apply styles to that
